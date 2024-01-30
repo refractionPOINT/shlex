99b15785baf9f9ec1454930132679a2e135d7a6a
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2024-01-30 10:21:26 -0800

    Applying various tweaks, tests and format.

--- shlex_test.go
+++ shlex_test.go
@@ -24,7 +24,15 @@ import (
 var (
 	// one two "three four" "five \"six\"" seven#eight # nine # ten
 	// eleven 'twelve\'
-	testString = "one two \"three four\" \"five \\\"six\\\"\" seven#eight # nine # ten\n eleven 'twelve\\' thirteen=13 fourteen/14"
+	testString          = "one two \"three four\" \"five \\\"six\\\"\" seven#eight # nine # ten\n eleven 'twelve\\' thirteen=13 fourteen/14"
+	testStringBackPort  = `run --command "hello \"some\value\\another\" there" finally`
+	testStringBackPort2 = `dir_list "c:\\" *`
+	testStringBackPort3 = `dir_list "c:\\ 'yup'" *`
+	testStringBackPort4 = `dir_list "c:\\ \"yup\"" *`
+	testStringBackPort5 = `log_get --file 'c:\temp\data.json' --type json`
+	testStringBackPort6 = `log_get --file c:\temp\data.json --type json`
+	testStringBackPort7 = `dir_list "c:\\ \'yup\'" *`
+	testStringBackPort8 = `history_dump`
 )
 
 func TestClassifier(t *testing.T) {
@@ -96,6 +104,134 @@ func TestSplit(t *testing.T) {
 	for i := range got {
 		if got[i] != want[i] {
 			t.Errorf("Split(%q)[%v] -> %v. Want: %v", testString, i, got[i], want[i])
+		}
+	}
+}
+
+func TestSplitBackSupport(t *testing.T) {
+	want := []string{"run", "--command", "hello \"some\\value\\another\" there", "finally"}
+	got, err := Split(testStringBackPort)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(want) != len(got) {
+		t.Errorf("Split(%q) -> %#v. Want: %#v", testStringBackPort, got, want)
+	}
+	for i := range got {
+		if got[i] != want[i] {
+			t.Errorf("Split(%q)[%v]\n->\n%#v.\nWant:\n%#v", testStringBackPort, i, got[i], want[i])
+		}
+	}
+}
+
+func TestSplitBackSupport2(t *testing.T) {
+	want := []string{"dir_list", "c:\\", "*"}
+	got, err := Split(testStringBackPort2)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(want) != len(got) {
+		t.Errorf("Split(%q) -> %#v. Want: %#v", testStringBackPort2, got, want)
+	}
+	for i := range got {
+		if got[i] != want[i] {
+			t.Errorf("Split(%q)[%v]\n->\n%#v.\nWant:\n%#v", testStringBackPort2, i, got[i], want[i])
+		}
+	}
+}
+
+func TestSplitBackSupport3(t *testing.T) {
+	want := []string{"dir_list", "c:\\ 'yup'", "*"}
+	got, err := Split(testStringBackPort3)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(want) != len(got) {
+		t.Errorf("Split(%q) -> %#v. Want: %#v", testStringBackPort3, got, want)
+	}
+	for i := range got {
+		if got[i] != want[i] {
+			t.Errorf("Split(%q)[%v]\n->\n%#v.\nWant:\n%#v", testStringBackPort3, i, got[i], want[i])
+		}
+	}
+}
+
+func TestSplitBackSupport4(t *testing.T) {
+	want := []string{"dir_list", "c:\\ \"yup\"", "*"}
+	got, err := Split(testStringBackPort4)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(want) != len(got) {
+		t.Errorf("Split(%q) -> %#v. Want: %#v", testStringBackPort4, got, want)
+	}
+	for i := range got {
+		if got[i] != want[i] {
+			t.Errorf("Split(%q)[%v]\n->\n%#v.\nWant:\n%#v", testStringBackPort4, i, got[i], want[i])
+		}
+	}
+}
+
+func TestSplitBackSupport5(t *testing.T) {
+	want := []string{"log_get", "--file", "c:\\temp\\data.json", "--type", "json"}
+	got, err := Split(testStringBackPort5)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(want) != len(got) {
+		t.Errorf("Split(%q) -> %#v. Want: %#v", testStringBackPort5, got, want)
+	}
+	for i := range got {
+		if got[i] != want[i] {
+			t.Errorf("Split(%q)[%v]\n->\n%#v.\nWant:\n%#v", testStringBackPort5, i, got[i], want[i])
+		}
+	}
+}
+
+func TestSplitBackSupport6(t *testing.T) {
+	want := []string{"log_get", "--file", "c:\\temp\\data.json", "--type", "json"}
+	got, err := Split(testStringBackPort6)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(want) != len(got) {
+		t.Errorf("Split(%q) -> %#v. Want: %#v", testStringBackPort6, got, want)
+	}
+	for i := range got {
+		if got[i] != want[i] {
+			t.Errorf("Split(%q)[%v]\n->\n%#v.\nWant:\n%#v", testStringBackPort6, i, got[i], want[i])
+		}
+	}
+}
+
+func TestSplitBackSupport7(t *testing.T) {
+	want := []string{"dir_list", "c:\\ 'yup'", "*"}
+	got, err := Split(testStringBackPort7)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(want) != len(got) {
+		t.Errorf("Split(%q) -> %#v. Want: %#v", testStringBackPort7, got, want)
+	}
+	for i := range got {
+		if got[i] != want[i] {
+			t.Errorf("Split(%q)[%v]\n->\n%#v.\nWant:\n%#v", testStringBackPort7, i, got[i], want[i])
+		}
+	}
+}
+
+func TestSplitBackSupport8(t *testing.T) {
+	want := []string{"history_dump"}
+	got, err := Split(testStringBackPort8)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(want) != len(got) {
+		t.Errorf("Split(%q) -> %#v. Want: %#v", testStringBackPort8, got, want)
+	}
+	for i := range got {
+		if got[i] != want[i] {
+			t.Errorf("Split(%q)[%v]\n->\n%#v.\nWant:\n%#v", testStringBackPort8, i, got[i], want[i])
 		}
 	}
 }
